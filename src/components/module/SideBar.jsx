6e1ffd623f1139188000b37200f264c545099da3
erfan nasrollahi <erfannasrolahi8@gmail.com>
2024-12-21 12:16:17 +0330

    End Project

--- src/components/module/SideBar.jsx
+++ src/components/module/SideBar.jsx
@@ -12,6 +12,7 @@ function SideBar() {
       <Link href="/buy-residential">همه</Link>
       {Object.keys(categories).map((i) => (
         <Link
+          key={i}
           href={{
             pathname: "/buy-residential",
             query: { category: i },
